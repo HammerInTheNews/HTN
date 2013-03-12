422e9c9451f4de4a9dde2236fc97754dfbf2fcc6
Heather Payne <heather@heatherpayne.ca>
2013-03-11 21:16:33 -0400

    last

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+    $(document).ready(function() {
+    	alert($(".truncate").length)
+    	$(".ellipsis").dotdotdot({
+    		height: 30
+    	});
+    });
