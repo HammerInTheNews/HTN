5284f1275ca55566ec2474e78ab8ae7ab66ff429
ajudson <alanjudson@gmail.com>
2013-03-14 20:14:28 -0400

    Deleting Users

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :name, :email
-
-  has_many :posts
-  has_many :comments
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,27 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130314154213) do
+ActiveRecord::Schema.define(:version => 20130309194308) do
 
   create_table "comments", :force => true do |t|
     t.string   "body"
     t.integer  "post_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "user_id"
   end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "user_id"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
