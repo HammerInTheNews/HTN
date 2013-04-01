63379899b28c3490ce197872f4c65a67b1e614da
ajudson <alanjudson@gmail.com>
2013-03-31 21:41:53 -0400

    Added amazon s3 credentials

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,5 +9,12 @@ class Post < ActiveRecord::Base
   has_many :images
   belongs_to :user
   has_attached_file :image, styles: { thumb: '70x60#', small: '150x150>', large: '680x340', ugly: '666x666!' },
-                     size: { :in => 0..1000.kilobytes }
+                     size: { :in => 0..1000.kilobytes },
+                                        storage: :s3,
+                                        s3_credentials: {
+                                        access_key_id: 'AKIAJQFILE7GM3P4IOUA',
+                                        secret_access_key: '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
+',
+                                        bucket: 'hammerinthenews'
+                                       }
 end
