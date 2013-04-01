31878ea470baefb875bc99ed687101a289d497b4
ajudson <alanjudson@gmail.com>
2013-03-31 20:23:34 -0400

    used paperclip with imagemagick - success with thumbnail photos

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,5 +8,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :images
   belongs_to :user
-  has_attached_file :image
+  has_attached_file :image, styles: { thumb: '70x60#', small: '150x150>', large: '680x340', ugly: '666x666!' },
+                     size: { :in => 0..1000.kilobytes }
 end
