981d3fb0e3efbdb4dd9c18dbd005fae59b54e5a8
ajudson <alanjudson@gmail.com>
2013-04-18 20:22:53 -0400

    fixed css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :setup_search
+  helper_method :recent_posts
 
   def setup_search
   	@q = Post.search(params[:q])
+  end
+
+  def recent_posts
+  	@recent_posts ||= Post.limit(5).order("created_at desc")
   end
 end
