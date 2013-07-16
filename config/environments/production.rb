6c3f6863d467d018d0ab316620a912d71e935459
Alan Judson <hammeringthenews@gmail.com>
2013-07-15 20:52:22 -0400

    made change to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,8 @@ HTN::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
-
+  #config.active_support.deprecation = :notify
+::ActiveSupport::Deprecation.silenced = true
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
